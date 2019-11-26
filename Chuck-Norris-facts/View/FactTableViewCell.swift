3d7ec9331ef3186d00874de0806243e79d6064dd
JBatistta1 <j.batista.damasceno@icloud.com>
2019-11-26 16:30:46 -0300

    Init File

---
+++ Chuck-Norris-facts/View/FactTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FactTableViewCell.swift
+//  Chuck-Norris-facts
+//
+//  Created by Joao Batista on 26/11/19.
+//  Copyright © 2019 Joao Batista. All rights reserved.
+//
+
+import UIKit
+
+class FactTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
